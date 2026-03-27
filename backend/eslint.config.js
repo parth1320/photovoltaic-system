432bd59000a0220fb5aa930f3d450e5cb308b4e2
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2026-03-28 00:08:42 +0100

    chore: add ESLint configuration and update package.json scripts

---
+++ backend/eslint.config.js
@@ -0,0 +1,23 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        exports: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        process: "readonly",
+        console: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+      },
+    },
+  },
+];
--- frontend/src/pages/project/ProjectDetails.js
+++ frontend/src/pages/project/ProjectDetails.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useReducer, useEffect, useCallback } from "react";
 import {
   Container,
   Row,
@@ -13,41 +13,53 @@ import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import axiosInstance from "../../axiosInstance/setHeader";
-// import VisualMap from "./VisualMap";
 import AddProductForm from "../product/AddProductForm";
+
+const initialState = {
+  project: {},
+  productNames: [],
+  showAddProductModal: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_PROJECT":
+      return { ...state, project: action.payload };
+    case "SET_PRODUCT_NAMES":
+      return { ...state, productNames: action.payload };
+    case "SHOW_ADD_MODAL":
+      return { ...state, showAddProductModal: true };
+    case "HIDE_ADD_MODAL":
+      return { ...state, showAddProductModal: false };
+    default:
+      return state;
+  }
+}
 
 const ProjectDetails = () => {
   const { projectId } = useParams();
-  const [project, setProject] = useState([]);
-  const [productNames, setProductNames] = useState([]);
-  const [showAddProductModal, setShowAddProductModal] = useState(false);
-  const [isEditProductModalVisible, setEditProductModalVisibility] =
-    useState(false);
-  const [editProductData, setEditProductData] = useState(null);
-
-  let formatedCreatedAt;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { project, productNames, showAddProductModal } = state;
 
   const fetchProject = useCallback(async () => {
     try {
       const response = await axiosInstance.get(
         `http://localhost:5000/project/${projectId}`,
       );
-      setProject(response.data);
+      dispatch({ type: "SET_PROJECT", payload: response.data });
     } catch (error) {
       console.error(error);
     }
   }, [projectId]);
 
-  const fetchProducts = async () => {
+  const fetchProducts = useCallback(async () => {
     try {
-      const response = await axiosInstance.get(
-        `http://localhost:5000/products`,
-      );
-      setProductNames(response.data);
+      const response = await axiosInstance.get(`http://localhost:5000/products`);
+      dispatch({ type: "SET_PRODUCT_NAMES", payload: response.data });
     } catch (error) {
       console.error(error);
     }
-  };
+  }, []);
 
   useEffect(() => {
     fetchProject();
@@ -55,16 +67,12 @@ const ProjectDetails = () => {
   }, [fetchProject, fetchProducts]);
 
   const handleCreateReport = async (projectId, productId) => {
-    // console.log(projectId, productId);
     try {
       const response = await axiosInstance.get(
         `http://localhost:5000/generate-report/${projectId}/${productId}`,
         { responseType: "arraybuffer" },
       );
-      // Creates a URL for the Blob data and download the PDF
-
       const blob = new Blob([response.data], { type: "application/pdf" });
-
       const url = URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.href = url;
@@ -91,17 +99,8 @@ const ProjectDetails = () => {
       } = productDetails;
       const response = await axiosInstance.post(
         `http://localhost:5000/${project._id}/products`,
-        {
-          name,
-          powerPeak,
-          orientation,
-          inclination,
-          area,
-          latitude,
-          longitude,
-        },
+        { name, powerPeak, orientation, inclination, area, latitude, longitude },
       );
-
       if (response.statusText === "Created") {
         toast.success("Product has been added successfully");
         fetchProducts();
@@ -133,16 +132,13 @@ const ProjectDetails = () => {
   const editProductHandler = () => {};
 
   let noProductMessage = null;
-
-  if (project.products) {
-    if (project.products.length === 0) {
-      noProductMessage = (
-        <Alert variant="info">
-          No products added. Please click "Add Product" to add products to this
-          project.
-        </Alert>
-      );
-    }
+  if (project.products && project.products.length === 0) {
+    noProductMessage = (
+      <Alert variant="info">
+        No products added. Please click "Add Product" to add products to this
+        project.
+      </Alert>
+    );
   }
 
   return (
@@ -150,7 +146,9 @@ const ProjectDetails = () => {
       <h1>Project: {project.name}</h1>
       <h4>Project description: {project.description}</h4>
 
-      <Button onClick={() => setShowAddProductModal(true)}>Add Product</Button>
+      <Button onClick={() => dispatch({ type: "SHOW_ADD_MODAL" })}>
+        Add Product
+      </Button>
       <Row>
         <Col>{noProductMessage}</Col>
       </Row>
@@ -218,7 +216,7 @@ const ProjectDetails = () => {
 
       <AddProductForm
         show={showAddProductModal}
-        onHide={() => setShowAddProductModal(false)}
+        onHide={() => dispatch({ type: "HIDE_ADD_MODAL" })}
         onAddProduct={handleAddProduct}
         productNames={productNames}
       />
@@ -227,187 +225,3 @@ const ProjectDetails = () => {
 };
 
 export default ProjectDetails;
-
-// import { useState, useEffect, useCallback } from "react";
-// import { useParams, Link } from "react-router-dom";
-// import { Container, Row, Col, Table, Button, Modal } from "react-bootstrap";
-// import { toast } from "react-toastify";
-
-// import axiosInstance from "../../axiosInstance/setHeader";
-// import CreateProjectForm from "./CreateProjectForm";
-
-// const ProjectDetails = () => {
-//   let formatedCreatedAt;
-//   const { projectId } = useParams();
-//   const [project, setProject] = useState(null);
-//   const [editMode, setEditMode] = useState(false);
-//   const [editData, setEditData] = useState(null);
-
-//   const fetchProject = useCallback(async () => {
-//     try {
-//       const response = await axiosInstance.get(
-//         `http://localhost:5000/project/${projectId}`,
-//       );
-//       setProject(response.data);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }, [projectId]);
-
-//   useEffect(() => {
-//     fetchProject();
-//   }, [fetchProject]);
-
-//   if (project) {
-//     const { createdAt } = project;
-
-//     const newdate = new Date(createdAt).toLocaleDateString("en-US", {
-//       day: "numeric",
-//       month: "long",
-//       year: "numeric",
-//     });
-
-//     formatedCreatedAt = newdate;
-//   }
-
-//   const handleCreateReport = async (projectId, productId) => {
-//     // console.log(projectId, productId);
-//     try {
-//       const response = await axiosInstance.get(
-//         `http://localhost:5000/generate-report/${projectId}/${productId}`,
-//         { responseType: "arraybuffer" },
-//       );
-//       // Creates a URL for the Blob data and download the PDF
-
-//       const blob = new Blob([response.data], { type: "application/pdf" });
-
-//       const url = URL.createObjectURL(blob);
-//       const link = document.createElement("a");
-//       link.href = url;
-//       link.setAttribute("download", "report.pdf");
-//       document.body.appendChild(link);
-//       link.click();
-//       document.body.removeChild(link);
-//     } catch (error) {
-//       console.error("Error creating report:", error);
-//       toast.error("Error while creating report...");
-//     }
-//   };
-
-//   const handleEditClick = (id, name, description, products) => {
-//     console.log(name, description, products);
-//     setEditMode(true);
-//     setEditData({ id, name, description, products });
-//   };
-
-//   const handleProjectUpdate = async (formData) => {
-//     console.log(`Updated Data: ${formData}`);
-//     fetchProject();
-//     setEditMode(false);
-//     setEditData(null);
-//   };
-
-//   if (!project) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <div>
-//       <Container>
-//         <Row className="mb-3">
-//           <Col>
-//             <h2>{project.name}</h2>
-//             <p>{project.description}</p>
-//             <Button
-//               variant="info"
-//               size="sm"
-//               onClick={() =>
-//                 handleEditClick(
-//                   project._id,
-//                   project.name,
-//                   project.description,
-//                   project.products,
-//                 )
-//               }
-//             >
-//               Edit Project
-//             </Button>
-//           </Col>
-//         </Row>
-//         <Row>
-//           <Col>
-//             <h3>Photovoltaic Products</h3>
-//             <Table striped bordered hover>
-//               <thead>
-//                 <tr>
-//                   <th>Product Name</th>
-//                   <th>createdAt</th>
-//                   <th style={{ width: "150px" }}>Action</th>
-//                 </tr>
-//               </thead>
-//               <tbody>
-//                 {project.products.map((product, index) => (
-//                   <tr key={index}>
-//                     <td>
-//                       <div>
-//                         <strong>{product.name}</strong>
-//                       </div>
-//                       <div style={{ fontSize: "12px" }}>
-//                         Area: {product.area} m² | Inclination:{" "}
-//                         {product.inclination}° | Orientation:{" "}
-//                         <strong>{product.orientation}</strong>
-//                       </div>
-//                     </td>
-//                     <td>{formatedCreatedAt}</td>
-//                     <td>
-//                       <Button
-//                         variant="primary"
-//                         onClick={() =>
-//                           handleCreateReport(project._id, product._id)
-//                         }
-//                       >
-//                         Create Report
-//                       </Button>
-//                     </td>
-//                   </tr>
-//                 ))}
-//               </tbody>
-//             </Table>
-//           </Col>
-//         </Row>
-
-//         <Row>
-//           <Link
-//             to={`/dashboard/visualmap/${project._id}`}
-//             className="btn btn-primary mt-3"
-//           >
-//             View Products on Visual Map
-//           </Link>
-//         </Row>
-
-//         <Modal show={editMode} onHide={() => setEditMode(false)}>
-//           <Modal.Header>
-//             <Modal.Title>Edit Project</Modal.Title>
-//           </Modal.Header>
-//           <Modal.Body>
-//             <CreateProjectForm
-//               editMode={editMode}
-//               initialData={editData}
-//               onSubmit={handleProjectUpdate}
-//             />
-//           </Modal.Body>
-//         </Modal>
-
-//         {/* {editMode && (
-//           <CreateProjectForm
-//             editMode={editMode}
-//             initialData={editData}
-//             onSubmit={handleProjectUpdate}
-//           />
-//         )} */}
-//       </Container>
-//     </div>
-//   );
-// };
-
-// export default ProjectDetails;
